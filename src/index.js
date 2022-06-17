88f0bcd504ad4b28aed67ccc33e1a61237ad05ec
bill apiyo <billian177@gmail.com>
2022-06-17 15:35:54 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const addNewTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+const createNewTask = (event) => {
+  event.preventDefault();
+  newTask.innerText = newTaskDescription.value;
+  addNewTask(newTask);
+  
+};
+
